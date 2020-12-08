c65b441144bd68ba23661bce760e58be6e37ecdf
milton <milton@miltons-MacBook.local>
2020-12-08 11:07:42 +0800

    add TouchableOpacity button to generate color

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
   const [colorArray, setColorArray] = useState([
-    { red: 255, green: 128, blue: 128, id: "0" },
-    { red: 0, green: 255, blue: 128, id: "1" },
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
     { red: 0, green: 0, blue: 255, id: "2" },
   ]);
 
   function renderItem({ item }) {
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
   }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`,
+      },
+    ]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "blue" }}>Add colour</Text>
+      </TouchableOpacity>
+      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -44,5 +74,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  list: {
+    width: "100%",
   },
 });
