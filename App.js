e63d0e071bcecdb92c90ec0f5d400de431af59c4
milton <milton@miltons-MacBook.local>
2020-12-08 10:48:00 +0800

    add Flatlist Array

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 128, blue: 128, id: "0" },
+    { red: 0, green: 255, blue: 128, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
   return (
     <View style={styles.container}>
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
+      <FlatList
+        style={{ width: "100%" }}
+        data={colorArray}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
